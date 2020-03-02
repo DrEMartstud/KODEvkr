47bbffdd5a1d8b7d5b67ee5b53350dfee39777d4
DrEMartstud <DrEMartstud@gmail.com>
2020-03-02 22:31:20 +0200

    Design+logic
    Implemented screen designs and started forking on their logic

---
+++ VKRwithSB/VKRDiplomaKODE/Extensions+Utilities/setCornerRadiusExtension.swift
@@ -0,0 +1,15 @@
+//
+//  setCornerRadiusExtension.swift
+//  VKRwithSB
+//
+//  Created by DrEMartstud on 02.03.2020.
+//  Copyright © 2020 DrEMartstud. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func setCornerRadius(r: CGFloat) {
+        self.layer.cornerRadius = r
+    }
+}
